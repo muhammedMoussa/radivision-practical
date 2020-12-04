7a508c0dbb907e42383c1758257f381aa32ac0b3
muhammedMoussa <muhammedmoussa@hotmail.com>
2020-12-04 16:47:27 +0200

    added outer effects

--- practical-2/src/index.js
+++ practical-2/src/index.js
@@ -5,3 +5,19 @@ document.querySelector('.outer__circle').addEventListener('mouseover', () => {
 document.querySelector('.outer__circle').addEventListener('mouseout', () => {
     document.querySelector('.outer__circle').classList.remove('outer__mouseover')
 });
+
+
+document.querySelector('.inner__circle').addEventListener('mouseover', () => {
+    document.querySelector('.inner__circle').classList.add('inner__mouseover')
+    document.querySelector('.inner__circle').classList.add('pointer')
+    document.querySelector('.outer__circle').classList.add('pointer')
+    document.querySelector('.outer__circle').classList.add('outer__mouseover__yellow')
+    
+});
+
+document.querySelector('.inner__circle').addEventListener('mouseout', () => {
+    document.querySelector('.inner__circle').classList.remove('inner__mouseover')
+    document.querySelector('.inner__circle').classList.remove('pointer')
+    document.querySelector('.outer__circle').classList.remove('pointer')
+    document.querySelector('.outer__circle').classList.remove('outer__mouseover__yellow')
+});
