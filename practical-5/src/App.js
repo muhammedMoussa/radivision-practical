26ac53ed0e8b555f964e01e7b2c5ad1fa61ff9d8
muhammedMoussa <muhammedmoussa@hotmail.com>
2020-12-04 19:09:18 +0200

    added Header

--- practical-5/src/App.js
+++ practical-5/src/App.js
@@ -1,9 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+
 import './App.css';
+import { api } from './shared/api';
 
 function App() {
+  const getMovies = async () => {
+    
+    const res = await fetch(`${api.url}/movie/upcoming${api.api_key}`);
+    const data = await res.json();
+    console.log(data);
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])  
+
   return (
-    <div className="App">
-      Hello!
+    <div className="">
+      <Header />
     </div>
   );
 }
---
+++ practical-5/src/shared/api.js
@@ -0,0 +1,5 @@
+export const api = {
+    url: 'https://api.themoviedb.org/3',
+    api_key: '?api_key=bc088fe71dd6571f1b571a32a23d025d',
+    backdrop_route: 'https://image.tmdb.org/t/p/original/'
+}
